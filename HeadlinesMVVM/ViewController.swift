246e853842d7aa17ab0a1ca8069765e42f306a4d
claire <claire@fsbtech.com>
2019-12-10 08:56:24 +0000

    Initial Commit

---
+++ HeadlinesMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeadlinesMVVM
+//
+//  Created by Claire Roughan on 10/12/2019.
+//  Copyright © 2019 Claire Roughan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
